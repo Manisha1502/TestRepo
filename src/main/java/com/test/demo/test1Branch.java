4623f539f30ed7b0cc0736569fa839c060d4d2ed
ManishaBundiwal <moonisha1@gmail.com>
2021-11-10 21:56:24 +0530

    created a new branch and added two new files

---
+++ src/main/java/com/test/demo/test1Branch.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class testBranchSecond {
+
+    public static void main(String[] args) {
+        System.out.println("Test1Branch");
+    }
+
+}
